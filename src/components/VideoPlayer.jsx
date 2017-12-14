1fcdeff6299165f8c88a8adee0c4d8e585ad619d
wongal5 <a.wong9970@gmail.com>
2017-12-14 12:23:59 -0800

    added view count, likes, dislikes

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,14 +1,48 @@
-let VideoPlayer = (props) => (
-  <div className="video-player">
-    <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.video.id.videoId}`} allowFullScreen></iframe>
-    </div>
-    <div className="video-player-details">
-      <h3>{props.video.snippet.title}</h3>
-      <div>{props.video.snippet.description}</div>
-    </div>
-  </div>
-);
+class VideoPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getVideoDetails = this.getVideoDetails.bind(this);
+    this.views;
+    this.likes;
+    this.dislikes;
+  }
+
+  getVideoDetails(videoId) {
+    let outerThis = this;
+    return $.ajax({
+      url: 'https://www.googleapis.com/youtube/v3/videos',
+      data: {
+        part: 'statistics',
+        key: window.YOUTUBE_API_KEY,
+        id: videoId,
+      },
+      success: function (data) {
+        outerThis.views = data.items[0].statistics.viewCount;
+        outerThis.likes = data.items[0].statistics.likeCount;
+        outerThis.dislikes = data.items[0].statistics.dislikeCount;
+      }
+    });
+  }
+
+  render() {
+    this.getVideoDetails(this.props.video.id.videoId);
+    return (
+      <div className="video-player">
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${this.props.video.id.videoId}`} allowFullScreen></iframe>
+        </div>
+        <div className="video-player-details">
+          <h3>{this.props.video.snippet.title}</h3>
+          <div className="featured-video-caption">{this.props.video.snippet.description}</div>
+          <div className="featured-video-caption"> Views: {new Intl.NumberFormat().format(this.views)} </div>
+          <div className="featured-video-caption"> Likes: {new Intl.NumberFormat().format(this.likes)} </div>
+          <div className="featured-video-caption"> Dislikes: {new Intl.NumberFormat().format(this.dislikes)} </div>
+        </div>
+      </div>
+    );
+  }
+
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
